2f30c8486a34d03dfdf5b6e7d7f8b85ff94ce02a
sowmya_kota <kotasowmya.1000@gmail.com>
2025-04-17 14:59:52 +0530

    delimiter

--- string-calculator/src/main/java/Calculator.java
+++ string-calculator/src/main/java/Calculator.java
@@ -1,13 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
 	public int add(String numbers) {
 		if(numbers.isEmpty()) {
 			return 0;
 		}
-		String[] numArray=numbers.replace("\n",",").split(",");
+		String delimiter=",|\n";
+		if(numbers.startsWith("//")) {
+			int delimiterIndex=numbers.indexOf("\n");
+			delimiter=numbers.substring(2, delimiterIndex);
+			numbers=numbers.substring(delimiterIndex+1);
+		}
+		
+		String[] tokens=numbers.split(delimiter);
+		List<Integer>negatives=new ArrayList<>();
 		int sum=0;
-		for(String num:numArray) {
-			sum+=Integer.parseInt(num.trim());
+		for(String token:tokens) {
+			if(!token.trim().isEmpty()) {
+			int num=Integer.parseInt(token.trim());
+			if(num<0) {
+				negatives.add(num);
+			}
+			sum+=num;
+			}
+		}
+		if(!negatives.isEmpty()) {
+			throw new IllegalArgumentException("Negative numbers are not allowed:"+negatives);
 		}
 		return sum;
 	}
--- string-calculator/src/test/java/CalculatorTest.java
+++ string-calculator/src/test/java/CalculatorTest.java
@@ -1,46 +1,46 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
+	private final Calculator calculator= new Calculator();
 	
 	@Test
 	public void testStringreturnZero() {
-	Calculator calculator=new Calculator();
-	int result=calculator.add("");
-	assertEquals(0, result);
+	assertEquals(0, calculator.add(""));
 	}
 	
 	@Test
 	public void testSingleNumber() {
-	Calculator calculator=new Calculator();
-	int result=calculator.add("1");
-	assertEquals(1, result);
+	assertEquals(1, calculator.add("1"));
 	}
 	
 	@Test
 	public void testTwoNumbers() {
-	Calculator calculator=new Calculator();
-	int result=calculator.add("1,5");
-	assertEquals(6, result);
+	assertEquals(6, calculator.add("1,5"));
 	}
 	
 	@Test
 	public void testMultipleNumbers() {
-	Calculator calculator=new Calculator();
-	int result=calculator.add("1,2,3,4");
-	assertEquals(10, result);
+	assertEquals(10, calculator.add("1,2,3,4"));
 	}
 	
 	@Test
 	public void testNewLineBetweenNumbers() {
-	Calculator calculator=new Calculator();
-	int result=calculator.add("1\n2,3");
-	assertEquals(6, result);
+	assertEquals(6, calculator.add("1\n2,3"));
 	}
 	
 	@Test
 	public void testNewLineAndCommaBetweenNumbers() {
-	Calculator calculator=new Calculator();
-	int result=calculator.add("1\n2,3\n4");
-	assertEquals(10, result);
+	assertEquals(10, calculator.add("1\n2,3\n4"));
 	}
+	
+	@Test
+	public void testDelimiter() {
+	assertEquals(3, calculator.add("//;\n1;2"));
+	}
+	
+	@Test
+	public void testMultipleDelimiter() {
+	assertEquals(10, calculator.add("//#\n1#2#3#4"));
+	}
+	
 }
