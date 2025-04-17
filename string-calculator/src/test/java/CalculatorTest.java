9768394384b47322bf1dedfa2a913254a3967a88
sowmya_kota <kotasowmya.1000@gmail.com>
2025-04-17 13:07:16 +0530

    handle single input

--- string-calculator/src/test/java/CalculatorTest.java
+++ string-calculator/src/test/java/CalculatorTest.java
@@ -4,7 +4,7 @@ public class CalculatorTest {
 	@Test
 	public void testString() {
 	Calculator calculator=new Calculator();
-	int result=calculator.add("");
-	assertEquals(0, result);
+	int result=calculator.add("1");
+	assertEquals(1, result);
 	}
 }
